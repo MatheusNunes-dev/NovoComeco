5b5faf7e77e1b3e789209d51ad019516184de126
Matheus Müller <MM-Muller@users.noreply.github.com>
2024-11-12 21:49:00 -0300

    db

--- db.php
+++ db.php
@@ -1,13 +1,24 @@
 <?php
-$servername = "localhost";  // ou o endereço do seu servidor MySQL
-$username = "root";         // nome de usuário do MySQL
-$password = "";             // senha do MySQL
-$dbname = "novocomeco";     // nome do banco de dados
+$servername = "novocomeco.mysql.database.azure.com";  // endereço do servidor no Azure
+$username = "novocomeco@novocomeco";                 // nome de usuário completo (usuário@servidor)
+$password = "novocomeco#2024";                       // senha do MySQL
+$dbname = "NovoComeco";                              // nome do banco de dados
+$port = 3306;                                        // porta padrão do MySQL
 
-// Cria a conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Caminho para o certificado SSL da CA (normalmente é baixado do Azure)
+$ca_cert_path = "/caminho/para/ca-cert.pem";
 
-// Verifica a conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+// Inicializa a conexão com SSL
+$con = mysqli_init();
+mysqli_ssl_set($con, NULL, NULL, $ca_cert_path, NULL, NULL);
+
+// Conecta ao banco de dados com SSL
+if (!mysqli_real_connect($con, $servername, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Falha na conexão: " . mysqli_connect_error());
 }
+
+echo "Conexão bem-sucedida com SSL!";
+
+// Fechar a conexão quando não for mais necessário
+mysqli_close($con);
+?>
